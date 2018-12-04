95115aaa127c3f3346ab592f42b165a2ccb1cd3c
Moses Surumen <moses.susrumen@gmail.com>
2018-12-03 20:43:40 -0800

    heroku

--- SomaLab/settings.py
+++ SomaLab/settings.py
@@ -138,12 +138,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join('staticfiles')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = [
-    os.path.join(STATIC_FILE_DIR_ROOT, 'static'),
+    os.path.join('static'),
 ]
 
 # Adding the sass finder to locate the generated .css file
